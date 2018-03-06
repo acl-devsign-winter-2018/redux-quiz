967fb28144b78207129305008be0d97ab9138908
Charlie Heiner <heinercj@gmail.com>
2018-03-06 14:26:38 -0800

    added display component to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Controls from './Controls';
+import Display from './Display';
 
 export default class App extends Component {
 
@@ -8,6 +9,7 @@ export default class App extends Component {
       <section>
         <div>Hello React</div>
         <Controls/>
+        <Display/>
       </section>
     );
   }
